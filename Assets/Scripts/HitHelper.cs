3e27a3d31a37b219a0b4174a311dbab42faf2916
deznod <fredomworld1@gmail.com>
2018-05-07 01:34:35 +0300

    Влад
    Данный класс отвечает за клик мыши, включениее анимации по клику мыши на врага,а так же за нанисение урона.

---
+++ Assets/Scripts/HitHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * Данный класс отвечает за клик мыши, включениее анимации по клику мыши на врага,а так же за нанисение урона.
+ */
+public class HitHelper : MonoBehaviour {
+
+    /**
+     *Метод обрабатывающий нажатие на клавишу мышы.
+     */
+     void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown()");// Требуется для отлаживания
+        GetComponent<Animator>().SetTrigger("Hit");// Включает анимацию по нажатию кнопки мыши по объекту 
+        GetComponent<HealthHelper>().GetHit(10);// наносит урон в 10
+    }
+
+
+}
